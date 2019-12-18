efd1ad506f3b8ae09407d2ebfb4c0955016cb949
Roman EBSurface <roman.jaeger@gmail.com>
2019-12-18 08:56:50 +0100

    replace address with company

--- qr.js
+++ qr.js
@@ -6,7 +6,7 @@ var company = encodeURIComponent(we.authMgr.getUser().profile.firma);
 var email = encodeURIComponent(we.authMgr.getUser().profile.publicEmailAddress.toLowerCase());
 var size = "&size=220x220&margin=0";
 
-var url = "https://api.qrserver.com/v1/create-qr-code/?data=BEGIN%3AVCARD%0AVERSION%3A2.1%0AFN%3A" + firstName + "+" + lastName + "%0AN%3A" + lastName + "%3B" + firstName + "%0ATEL%3BWORK%3BVOICE%3A" + phone + "%0AEMAIL%3BWORK%3BINTERNET%3A" + email + "%0AADR%3BWORK%3A%3B%3B" + company +"%0AEND%3AVCARD%0A" + size + color; 
+var url = "https://api.qrserver.com/v1/create-qr-code/?data=BEGIN%3AVCARD%0AVERSION%3A2.1%0AFN%3A" + firstName + "+" + lastName + "%0AN%3A" + lastName + "%3B" + firstName + "%0ATEL%3BWORK%3BVOICE%3A" + phone + "%0AEMAIL%3BWORK%3BINTERNET%3A" + email + "%0AORG%3A" + company +"%0AEND%3AVCARD%0A" + size + color; 
 
 var qrSelector = document.querySelector('.external-script-widget[data-widget-id="qrcode"]');
 
