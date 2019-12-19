69566f7084b287f43bc1994bbdd36b65ebb306cf
Roman EBSurface <roman.jaeger@gmail.com>
2019-12-19 14:57:50 +0100

    lalala

--- qr.js
+++ qr.js
@@ -5,14 +5,14 @@ var phone = encodeURIComponent(we.authMgr.getUser().phoneNumber);
 var company = encodeURIComponent(we.authMgr.getUser().profile.firma);
 var email = encodeURIComponent(we.authMgr.getUser().profile.publicEmailAddress.toLowerCase());
 var size = "&size=220x220&margin=0";
-
-if(phone in window == false) {
+/*
+if(phone in window) {
     phone = "";
     }
-if(email in window == false) {
+if(email in window) {
     email = "";
 }
-
+*/
 var url = "https://api.qrserver.com/v1/create-qr-code/?data=BEGIN%3AVCARD%0AVERSION%3A2.1%0AFN%3A" + firstName + "+" + lastName + "%0AN%3A" + lastName + "%3B" + firstName + "%0ATEL%3BWORK%3BVOICE%3A" + phone + "%0AEMAIL%3BWORK%3BINTERNET%3A" + email + "%0AORG%3A" + company +"%0AEND%3AVCARD%0A" + size + color; 
 
 var qrSelector = document.querySelector('.external-script-widget[data-widget-id="qrcode"]');
