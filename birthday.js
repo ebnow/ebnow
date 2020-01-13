469e5d4802ba6928911c9adaa1eccd6fa49d38d1
Roman EBSurface <roman.jaeger@gmail.com>
2020-01-13 11:22:46 +0100

    add emoji

--- birthday.js
+++ birthday.js
@@ -21,7 +21,7 @@ request.addEventListener('load', function(event) {
                 if(user.entityType == USER_ENTITY){
                    // User has birthday today
                    if(user.profile.geburtsdatum.substring(0, 6) == currentDate) {
-                    birthdayMessage += '<a href="/profile/' + user.id + '">' + user.firstName + ' ' + user.lastName + '</a> hat heute Geburtstag!</br>';
+                    birthdayMessage += '<a href="/profile/' + user.id + '">' + user.firstName + ' ' + user.lastName + '</a> hat heute Geburtstag! 🎉</br>';
                    }
                 }
             });
