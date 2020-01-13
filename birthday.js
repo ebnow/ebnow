35a69290313e36d4b599f00123ca54993753ed6b
Roman EBSurface <roman.jaeger@gmail.com>
2020-01-13 11:35:35 +0100

    wrap function

--- birthday.js
+++ birthday.js
@@ -1,42 +1,47 @@
-const today = new Date();
-const url = "https://de.eyo.net/api/users";
-const id = we.authMgr.getSessionID();
-
-const currentDate = (today.getDate() <= 9 ? "0" : "" ) + today.getDate() + "." + ((today.getMonth() + 1) <= 9 ? "0" : "" ) + (today.getMonth() + 1) + ".";
-
-const USER_ENTITY = "user"
-var birthdayRequest = new XMLHttpRequest();
-
-birthdayRequest.open("GET",url + ";wesessid=" + id + "?query=" + currentDate + "&limit=5&extended=false");
-birthdayRequest.addEventListener('load', function(event) {
-   if (birthdayRequest.status >= 200 && birthdayRequest.status < 300) {
-         let response = JSON.parse(birthdayRequest.responseText);
-         const data = response.data;
-
-         let birthdayMessage = "";
-        
-         if(data) {
-            data.forEach(user => {
-                // check if search hit is a user
-                if(user.entityType == USER_ENTITY){
-                   // User has birthday today
-                   if(user.profile.geburtsdatum.substring(0, 6) == currentDate) {
-                    birthdayMessage += '<a href="/profile/' + user.id + '">' + user.firstName + ' ' + user.lastName + '</a> hat heute Geburtstag! 🎉</br>';
-                   }
-                }
-            });
-
-            var greetingSelector = document.querySelector('.external-script-widget[data-widget-id="birthday"]');
-            //var profileLink = "/profile/" + we.authMgr.getUser().id;
+var loadBirthday = function () {
+    const today = new Date();
+    const url = "https://de.eyo.net/api/users";
+    const id = we.authMgr.getSessionID();
+    
+    const currentDate = (today.getDate() <= 9 ? "0" : "" ) + today.getDate() + "." + ((today.getMonth() + 1) <= 9 ? "0" : "" ) + (today.getMonth() + 1) + ".";
+    
+    const USER_ENTITY = "user"
+    const birthdayRequest = new XMLHttpRequest();
+    
+    birthdayRequest.open("GET",url + ";wesessid=" + id + "?query=" + currentDate + "&limit=5&extended=false");
+    birthdayRequest.addEventListener('load', function(event) {
+       if (birthdayRequest.status >= 200 && birthdayRequest.status < 300) {
+             let response = JSON.parse(birthdayRequest.responseText);
+             const data = response.data;
+    
+             let birthdayMessage = "";
             
+             if(data) {
+                data.forEach(user => {
+                    // check if search hit is a user
+                    if(user.entityType == USER_ENTITY){
+                       // User has birthday today
+                       if(user.profile.geburtsdatum.substring(0, 6) == currentDate) {
+                        birthdayMessage += '<a href="/profile/' + user.id + '">' + user.firstName + ' ' + user.lastName + '</a> hat heute Geburtstag! 🎉</br>';
+                       }
+                    }
+                });
+    
+                var greetingSelector = document.querySelector('.external-script-widget[data-widget-id="birthday"]');
+                //var profileLink = "/profile/" + we.authMgr.getUser().id;
+                
+    
+                if (greetingSelector) {
+                    greetingSelector.innerHTML = birthdayMessage;
+                } else {
+                    console.log("This didn't work. Try again");
+                };
+             }
+        }
+    });
+    
+    birthdayRequest.send();
+};
 
-            if (greetingSelector) {
-                greetingSelector.innerHTML = birthdayMessage;
-            } else {
-                console.log("This didn't work. Try again");
-            };
-         }
-    }
-});
+loadBirthday();
 
-birthdayRequest.send();
