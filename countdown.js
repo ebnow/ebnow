3c164a2335d8c506bf3348ef1f7155dcd18bd57f
IT-Unternehmensentwicklung <ue@MacBook-Pro.localdomain>
2020-01-31 14:23:20 +0100

    change countdown message

--- countdown.js
+++ countdown.js
@@ -17,8 +17,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="countdown"
-  document.getElementById("countdown").innerHTML = days + "Tage " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  var countdownMessage = "Noch " + days + "T " + hours + "h " + minutes + "m " + seconds + "s bis zur IT Cloud";
+  document.getElementById("countdown").innerHTML = countdownMessage;
 
   // If the count down is finished, write some text
   if (distance < 0) {
