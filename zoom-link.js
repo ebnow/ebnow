6e2d072c9eca86b4955f37a253e5c4f4e467f999
Janosch Schwalm <janosch.schwalm@gmail.com>
2020-12-10 16:30:02 +0100

    Update zoom-link.js (#4)
    make input smaller
    make button smaller
    center input and button
    bottom margin of input

--- zoom-link.js
+++ zoom-link.js
@@ -1,11 +1,16 @@
 inputForZoomId = document.createElement('input');
+inputForZoomId.placeholder = 'Besprechungs-ID';
+inputForZoomId.style.width = '200px';
+inputForZoomId.style.marginBottom = '5px';
 
 buttonForZoomId = document.createElement('button');
-buttonForZoomId.textContent = 'Zoom öffnen'
+buttonForZoomId.textContent = 'Zoom öffnen';
+buttonForZoomId.style.width = '118.562px';
 buttonForZoomId.onclick = function() {
   window.open('https://zoom.us/wc/join/' + inputForZoomId.value, '_blank');
 }
 
 linkGenerator = document.getElementById('link-generator');
+linkGenerator.style.textAlign = 'center';
 linkGenerator.appendChild(inputForZoomId);
 linkGenerator.appendChild(buttonForZoomId);
